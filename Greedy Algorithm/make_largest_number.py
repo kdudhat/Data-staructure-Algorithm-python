fd4db0adfea55a6943c12ae065393ff7596dae55
Kishan Dudhat <kishandudhat412@gmail.com>
2020-08-26 13:19:09 +0530

    Update make_largest_number.py

--- Greedy Algorithm/make_largest_number.py
+++ Greedy Algorithm/make_largest_number.py
@@ -1,3 +1,25 @@
+'''
+Input Format. 
+            The first line of the input contains an integer 𝑛. The second line contains integers 𝑎1, 𝑎2, . . . , 𝑎𝑛.
+
+Output Format.
+             Output the largest number that can be composed out of 𝑎1, 𝑎2, . . . , 𝑎𝑛.
+Sample 1.
+Input:
+2
+21 2
+Output:
+221
+
+Sample 2.
+Input:
+5
+9 4 6 1 9
+Output:
+99641
+
+
+'''
 def make_largest_number(n,digits):
     digits.sort()
     digits.sort(key=lambda x: int(x[0]),reverse=True)

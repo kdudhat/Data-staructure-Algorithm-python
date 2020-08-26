25f589095ce7a36c0d770b22661321eb01125166
Kishan Dudhat <kishandudhat412@gmail.com>
2020-08-26 11:27:20 +0530

    Update money_change.py

--- Greedy Algorithm/money_change.py
+++ Greedy Algorithm/money_change.py
@@ -1,3 +1,20 @@
+'''
+The goal in this problem is to find the minimum number of coins needed to change the input value
+(an integer) into coins with denominations 1, 5, and 10.
+
+Sample 1.
+Input:
+2
+Output:
+2
+2 = 1 + 1.
+Sample 2.
+Input:
+28
+Output:
+6
+
+'''
 amount = int(input())
 coin = 0
 if amount>=10:
